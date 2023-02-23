4e4f76684a0c2c2e07e1ceca4cff973d867c41e2
cinob <cinob@foxmail.com>
2023-02-23 17:39:49 +0800

    chore: init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import OSS from 'ali-oss'
+const client = new OSS({
+  accessKeyId: '123456',
+  accessKeySecret: 'asdasdasd',
+})
+</script>
+
+<template>
+  <div>
+    1
+  </div>
+</template>
